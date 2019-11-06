66190ae2536f8d8686b6d19522202de299b254d1
stan05 <stanislav.petr7@gmail.com>
2019-11-06 01:57:13 +0100

    addded login, logout, sing up feature

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //all required libraries
-var express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    Campground = require("./models/campground"),
-    seedDB = require("./seeds")
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment"),
+    User        = require("./models/user"),
+    seedDB      = require("./seeds")
 
 
 seedDB()
@@ -13,53 +17,75 @@ mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true, useUn
 
 //setting for YelpCamp project
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//PASSPORT configuration
+app.use(require("express-session")({
+    secret: "This one is secret",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+//comes all from passport-local-mongoose package
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
+//this middleware will run on every single route
+//pass user data
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
+// redirects to INDEX
 app.get("/", function(req, res){
     res.redirect("/campgrounds");
 });
 
-//FORM FOR ADD A NEW CAMPGROUND
-app.post("/campgrounds", function(req, res){
+//INDEX homepage for campgrounds
+app.get("/campgrounds", function(req, res){
+    //create a new campground ans save to DB
+    Campground.find({}, function (err, allCamps) {
+        console.log(req.user);
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {camps:allCamps, currentUser:req.user});
+        }
+    });
+});
+
+//NEW display form for a new campground
+app.get("/campgrounds/add", function (req, res) {
+    res.render("campgrounds/new");
+});
+
+//CREATE add a new campground to DB
+app.post("/campgrounds", function (req, res) {
     //get data from form and add to camps array
     var name = req.body.campName;
     var image = req.body.campImg;
     var desc = req.body.campDesc;
-    
-    Campground.create({
-        name:name,
-        image:image,
-        description:desc
 
-    }, function(err, newCamp){
-        if(err){
+    Campground.create({
+        name: name,
+        image: image,
+        description: desc
+
+    }, function (err, newCamp) {
+        if (err) {
             console.log(err);
-        } else{
+        } else {
             console.log("CREATED:" + newCamp);
             res.redirect("/campgrounds");
         }
     });
 });
-//INDEX(HOME) PAGE CAMPGROUNDS
-app.get("/campgrounds", function(req, res){
-    //create a new campground ans save to DB
-    Campground.find({}, function (err, allCamps) {
-        if(err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds-index", {camps:allCamps});
-        }
-    });
-});
-//ADD A CAMPGROUND PAGE
-app.get("/campgrounds/add", function(req, res){
-    res.render("campgrounds-add");
-});
 
-//SHOW campground route
+//SHOW shows a specific campground
 app.get("/campgrounds/:id", function(req, res){ 
     Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
         if(err){
@@ -67,14 +93,96 @@ app.get("/campgrounds/:id", function(req, res){
         } else{
             console.log(foundCampground.comments)
             //change in future to campground all to make more sense + cleaner code
-            res.render("campgrounds-show", {camp: foundCampground});
+            res.render("campgrounds/show", {camp: foundCampground});
         }
     });    
 });
 
+// ============
+// COMMENTS ROUTES
+// ============
 
 
+// NEW form for a comment
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function (req, res) {
+    console.log("not done yet");
+    
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("comments/new", { camp: campground });
+        }
+    });
+});
 
+// CREATE add a new comment do DB
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else{
+            // create a new comment
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    console.log("added sucessfuly");
+                    res.redirect("/campgrounds" + campground._id);
+                }
+            });
+        }
+    });
+    
+});
+// ==============
+// AUTH ROUTES
+// ==============
+
+//show register form
+app.get("/register", function(req, res){
+    res.render("registration/register");
+});
+//sing up route for register
+app.post("/register", function(req, res){
+    var newUser = new User({username:req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("registration/register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");   
+        });
+    });
+});
+//show logic form
+app.get("/login", function(req, res){
+    res.render("registration/login");
+});
+//login route logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+//logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds");
+});
+
+//authentication security
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    };
+    res.redirect("/login");
+};
 
 app.listen(3000, function(){
     console.log("You have launched YelpCamp! Well done");
--- seeds.js
+++ seeds.js
@@ -4,49 +4,71 @@ var mongoose = require("mongoose"),
 
 
 var camps = [
-    { name: "Norway-Fjord", image: "https://www.fjordnorway.com/imageresizer/?image=%2Fdbimgs%2Fsande-camping-turid-sande-beinnes.jpg&action=Background_Overlay", description: "A very cool camp" },
-    { name: "Camping De Molignon", image: "https://static.alanrogers.com/images/generated/3d782eeb65eebd6b/CH9670-info-01_720_540_75_s_c1.jpg", description: "A very cool camp" },
-    { name: "Peruvian Andes", image: "https://previews.123rf.com/images/belikova/belikova1802/belikova180200006/94623178-camping-in-the-peruvian-andes-salkantay-trekking-peru-.jpg", description: "A very cool camp" },
-    { name: "Lagos de Somiedo", image: "https://about-spain.net/tourism/photos/camping-spain.jpg", description: "A very cool camp" },
-    { name: "L'Amfora", image: "https://static.alanrogers.com/images/generated/f7814f82205277e5/ES80350-info-01_720_540_75_s_c1.jpg", description: "A very cool camp" },
-    { name: "Norway-Fjord", image: "https://www.fjordnorway.com/imageresizer/?image=%2Fdbimgs%2Fsande-camping-turid-sande-beinnes.jpg&action=Background_Overlay", description: "A very cool camp" },
-    { name: "Camping De Molignon", image: "https://static.alanrogers.com/images/generated/3d782eeb65eebd6b/CH9670-info-01_720_540_75_s_c1.jpg", description: "A very cool camp" }
+    { name: "Norway-Fjord", image: "https://www.fjordnorway.com/imageresizer/?image=%2Fdbimgs%2Fsande-camping-turid-sande-beinnes.jpg&action=Background_Overlay", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." },
+    { name: "Camping De Molignon", image: "https://static.alanrogers.com/images/generated/3d782eeb65eebd6b/CH9670-info-01_720_540_75_s_c1.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." },
+    { name: "Peruvian Andes", image: "https://previews.123rf.com/images/belikova/belikova1802/belikova180200006/94623178-camping-in-the-peruvian-andes-salkantay-trekking-peru-.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." },
+    { name: "Lagos de Somiedo", image: "https://about-spain.net/tourism/photos/camping-spain.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." },
+    { name: "L'Amfora", image: "https://static.alanrogers.com/images/generated/f7814f82205277e5/ES80350-info-01_720_540_75_s_c1.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." },
+    { name: "Norway-Fjord", image: "https://www.fjordnorway.com/imageresizer/?image=%2Fdbimgs%2Fsande-camping-turid-sande-beinnes.jpg&action=Background_Overlay", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus. very cool camp" },
+    { name: "Camping De Molignon", image: "https://static.alanrogers.com/images/generated/3d782eeb65eebd6b/CH9670-info-01_720_540_75_s_c1.jpg", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam possimus odit optio a ipsum inventore facilis sequi nulla voluptates animi ratione, cum ea recusandae et hic, reprehenderit incidunt, iusto fuga quae ab. Quas, culpa assumenda at qui minus ipsum deleniti illo in sunt commodi saepe odit? Aliquid magnam eum quam vero nostrum nam esse hic facere ipsa. Maxime nemo inventore hic, natus delectus id beatae sunt cumque veritatis? Esse at fugiat dolorem nihil laborum voluptate ipsum ut error doloribus nesciunt eaque minima optio, soluta numquam! Molestiae non cumque ratione praesentium odio enim nemo molestias fugit expedita sequi, obcaecati qui temporibus." }
 ]; 
 
 
-function seedDB(){
-    // remove all camps
-    Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        } 
-        console.log("removed campgrounds");
-
-        // add new camps
-        camps.forEach(function (camp) {
-            Campground.create(camp, function (err, campground) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("added camp");
-                    Comment.create(
-                        {   
-                            text: "Some random comment",
-                            author: "Homer"
-                    }, function(err, comment){
-                        if(err){
-                            console.log(err);
-                        } else{
-                            campground.comments.push(comment);
-                            campground.save();
-                            console.log("added comment");
-                        }
-                    });
+async function seedDB() {
+    try {
+        // remove all camps and comments
+        await Comment.remove({});
+        await Campground.remove({});
+        for (const camp of camps) {
+            // create and push camps to DB + add a comment
+            let campground = await Campground.create(camp);
+            let comment = await Comment.create(
+                {
+                    author: "Homer",
+                    text: "This place sucks"
                 }
-            });
-        });
-    });
+            )
+            campground.comments.push(comment);
+            campground.save();
+        }
+    } catch(err){
+        console.log(err);
+    }
 }
+
+// function seedDB(){
+//     // remove all camps
+//     Campground.remove({}, function(err){
+//         if(err){
+//             console.log(err);
+//         } 
+//         console.log("removed campgrounds");
+
+//         // add new camps
+//         camps.forEach(function (camp) {
+//             Campground.create(camp, function (err, campground) {
+//                 if (err) {
+//                     console.log(err);
+//                 } else {
+//                     console.log("added camp");
+//                     Comment.create(
+//                         {   
+//                             text: "Some random comment",
+//                             author: "Homer"
+//                     }, function(err, comment){
+//                         if(err){
+//                             console.log(err);
+//                         } else{
+//                             campground.comments.push(comment);
+//                             campground.save();
+//                             console.log("added comment");
+//                         }
+//                     });
+//                 }
+//             });
+//         });
+//     });
+// }
     
 
 module.exports = seedDB;
