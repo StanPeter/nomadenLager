6c66bd8b31e0e560dafb5edc0fc05755d31fc49f
stan05 <stanislav.petr7@gmail.com>
2019-11-07 19:49:35 +0100

    added Edit, Remove functionality to comments

--- routes/comments.js
+++ routes/comments.js
@@ -49,20 +49,37 @@ router.post("/", isLoggedIn, function(req, res) {
 
 //EDIT comment
 router.get("/:comment_id/edit", function(req, res){
-    res.send("you made it bro");
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.render("comments/edit", { campground_id: req.params.id, comment:foundComment});
+        }
+    });
 });
 
 //UPDATE comment
 router.put("/:comment_id", function(req, res){
-    res.send("updated");
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.send("ERROR");
+        } else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
 //DESTROY comment
-router.delete("/:comment_id/delete", function(req, res){
-    res.send("deleted");
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
 });
 
-app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 //authentication security middleware
