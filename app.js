69a2b870c634b40dc5a9b98a6740694bd2b68ccd
stan05 <stanislav.petr7@gmail.com>
2019-11-19 03:23:15 +0100

    heroku now working, added some env vars

--- app.js
+++ app.js
@@ -63,10 +63,8 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 
-console.log(process.env.IP);
-
 
 app.listen(process.env.PORT, process.env.IP, function () {
-    console.log("The YelpCamp Server Has Started!");
+    console.log("Listening to port localhost:3000");
 });
 
