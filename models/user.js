b74844ef3b6677f321e4ddbca309288a4c9e5c57
stan05 <stanislav.petr7@gmail.com>
2019-12-09 21:29:14 +0100

    started feature reset password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
-
+var bcrypt = require("bcrypt-nodejs");
 
 var UserSchema = new mongoose.Schema({
     username: String,
@@ -8,7 +8,9 @@ var UserSchema = new mongoose.Schema({
     avatar: String,
     firstName: String,
     lastName: String,
-    email: String,
+    email: {type: String, unique: true, required: true},
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
     isAdmin: {type: Boolean, default: false}
 });
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router      = express.Router();
 var passport    = require("passport");
 var User        = require("../models/user");
 var Campground  = require("../models/campground");
+var async       = require("async");
+var nodemailer  = require("nodemailer");
+var crypto      = require("crypto"); //part of express
 
 
 //redirect to index(campgrounds) route
@@ -80,9 +83,66 @@ router.get("/logout", function (req, res) {
     res.redirect("/campgrounds");
 });
 
+//forgot password NEW
+router.get("/forgot", function(req, res) {
+    res.render("registration/forgot");
+});
 
+//forgot password CREATE
+router.post("/forgot", function(req, res, next){
+    async.waterfall([
+        //creating random token of 20length 
+        function(done) {
+            crypto.randomBytes(20, function(err, buf) {
+                var token = buf.toString("hex");
+                done(err, token);
+            });
+        },
+        //passing token to the user and setting for 1hour
+        function(token, done) {
+            User.findOne({ email: req.body.email }, function(err, user) {
+                if(!user) {
+                    req.flash("error", "No account with that email address exists");
+                    return res.redirect("/forgot");
+                }
+                user.resetPasswordToken = token;
+                user.resetPasswordExpires = Date.now() + 3600000; //1 hour
 
-module.exports = router;
+                user.save(function(err) {
+                    done(err, token, user);
+                });
+            });
+        },
+        //email automat to send the forgotten password link
+        function(token, user, done) {
+            var smtpTransport = nodemailer.createTransport({
+                service: "Gmail",
+                auth: {
+                    user: "bestcampground@gmail.com",
+                    pass: process.env.CAMPGROUNDPASSWORD
+                }
+            });
+            var mailOptions = {
+                to: user.email,
+                from: "bestcampground@gmail.com",
+                subject: "Forgotten password from your campground",
+                text: 'You are receiving this because you have requested the reset of the password for your account.\n\n' +
+                    'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+                    'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+                    'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+            };
+            smtpTransport.sendMail(mailOptions, function (err) {
+                console.log("mail sent");
+                req.flash("success", "An e-mail has been sent to ' + user.email + ' with further instructions.");
+                done(err, "done");
+            });
+        }
+    ], function(err) {
+        if (err) return next(err);
+        res.redirect('/forgot');
+    });
+});
+
 
 //USER ROUTES
 router.get("/users/:id", function(req, res){
@@ -100,3 +160,9 @@ router.get("/users/:id", function(req, res){
         });
     });
 });
+
+
+
+
+
+module.exports = router;
